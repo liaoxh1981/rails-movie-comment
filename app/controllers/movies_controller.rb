ec8a0f99705ea63e477d26beb7220a4c74277c38
廖晓华“git config --global user.email “liaoxh@gmail.com <liaoxh1981@gmail.com>
2017-04-03 18:18:23 +0800

    implement movies#update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,6 +22,14 @@ def create
   redirect_to movies_path
 end
 
+def update
+  @movie = Movie.find(params[:id])
+
+  @movie.update(movie_params)
+
+redirect_to movies_path, notice: "Update Success"
+end
+
 private
 
 def movie_params
