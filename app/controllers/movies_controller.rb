534fd7231eaf77a8de8e775b61c14dfd5a3f537d
廖晓华“git config --global user.email “liaoxh@gmail.com <liaoxh1981@gmail.com>
2017-04-03 17:45:35 +0800

    implement movies#create

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,18 @@ end
 def new
   @movie = Movie.new
 end
+
+def create
+  @movie = Movie.new(movie_params)
+  @movie.save
+
+  redirect_to movies_path
+end
+
+private
+
+def movie_params
+  params.require(:movie).permit(:movie, :information)
+end
+
 end
