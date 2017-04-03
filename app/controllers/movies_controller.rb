38ca09d2bbb1e55ee8d4b4587b595429299e049d
廖晓华“git config --global user.email “liaoxh@gmail.com <liaoxh1981@gmail.com>
2017-04-03 20:27:31 +0800

    add validation to title

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,9 +17,12 @@ end
 
 def create
   @movie = Movie.new(movie_params)
-  @movie.save
 
-  redirect_to movies_path
+  if @movie.save
+    redirect_to movies_path
+  else
+    render :new
+  end
 end
 
 def update
@@ -40,7 +43,7 @@ end
 private
 
 def movie_params
-  params.require(:movie).permit(:movie, :information)
+  params.require(:movie).permit(:title, :information)
 end
 
 end
