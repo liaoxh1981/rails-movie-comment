f1bdec92be928383434b35922adf7635ef463b22
廖晓华“git config --global user.email “liaoxh@gmail.com <liaoxh1981@gmail.com>
2017-04-04 14:09:59 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,11 @@ class User < ApplicationRecord
 def is_member_of?(movie)
   participated_movies.include?(movie)
 end
+
+def join!(movie)
+  participated_movies << movie
+end
+def quit!(movie)
+  participated_movies.delete(movie)
+end
 end
