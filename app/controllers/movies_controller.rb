50ec2a5768e244eac125c078d3f4ba3d9e6ade4b
廖晓华“git config --global user.email “liaoxh@gmail.com <liaoxh1981@gmail.com>
2017-04-04 14:40:13 +0800

    join & quit group action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join, :quit]
   before_action :find_movie_and_check_permission, only: [:edit, :update, :destroy]
 def index
   @movies = Movie.all
@@ -39,6 +39,30 @@ end
 def destroy
   @movie.destroy
   redirect_to movies_path, alert: "Movie deleted"
+end
+
+def join
+  @movie = Movie.find(params[:id])
+
+  if !current_user.is_member_of?(@movie)
+    current_user.join!(@movie)
+    flash[:notice] = "收藏本电影成功！"
+  else
+    flash[:warning] = "你已经收藏本电影了！"
+  end
+redirect_to movie_path(@movie)
+end
+
+def quit
+  @movie = Movie.find(params[:id])
+
+  if current_user.is_member_of?(@movie)
+    current_user.quit!(@movie)
+    flash[:alert] = "已取消收藏本电影！"
+  else
+    flash[:warning] = "你未收藏本电影，怎么退出 XD"
+  end
+  redirect_to movie_path(@movie)
 end
 
 private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :movies do
+    member do
+      post :join
+      post :quit
+    end
+
     resources :reviews
   end
+
   root 'movies#index'
 end
